2c6a19cb71d28e22b0c10c9361c643e5ef4c4d03
Alejandro Reyes <alex.re95@gmail.com>
2021-01-27 09:05:32 -0600

    Updated README, added plot title, updated sample frequency factor from 7 down to 5

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ This repository contains classwork for *Statistical Methods*, PHYS 5394.
 
 # Description
 
-* This repository is currently empty.
-* Check out [my GitHub](https://github.com/alexraevn/)!
+* **lab2**
+  - gensgsig.m     - Generate a sine-gaussian signal
+  - testgensgsig.m - Test the sine-gaussian signal and plot it
+
+Contact alejandro.reyes01@utrgv.edu
--- lab2/testgensgsig.m
+++ lab2/testgensgsig.m
@@ -1,13 +1,13 @@
 %% Plot the sine-gaussian signal
 % Signal parameters
-time0 = 0.5;
-stDev = 0.15;
+time0 = 0.5; % Peak chosen at the center
+stDev = 0.15; 
 freq0 = 12;
 phi0 = pi/2.0;
 A = 9;
 
 % Sample interval calculation
-samplFreq = 7*sqrt(5)*freq0; % Irrational number used to remove cyclic patterns in datapoints
+samplFreq = 5*sqrt(5)*freq0; % Irrational factor chosen to remove cyclic patterns in datapoints
 samplInterval = 1/samplFreq;
 
 % Time samples vector
@@ -19,4 +19,5 @@ sigVec = gensgsig(timeVec,A,time0,stDev,freq0,phi0);
 % Plot the signal
 figure;
 plot(timeVec,sigVec,'Marker','.','MarkerSize',24);
+title('Sine-Gaussian Signal')
 
