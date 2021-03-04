a986af2d98db977367c0585fb2930058d5f75f18
Alejandro Reyes <alex.re95@gmail.com>
2021-03-04 07:12:31 -0600

    testcustomprng.m now plots PDF instead of trial vector

--- lab7/testcustomprng.m
+++ lab7/testcustomprng.m
@@ -1,22 +1,36 @@
 %% Test the customrand and customrandn functions
 %% Generate probability distributions
-% Set the number of trials for each probability distribution
+% Set the number of trials for each trial run
 trials = 10000;
+% Declare PDF parameters
+a = -2;
+b = 1;
+m = 1.5; %mean
+s = 2.0; %std
 
 for i = 1:trials;
-% Generate the uniform PDF vector of length TRIALS
-uPDF(i) = customrand(-2,1); % Between -2,1
+% Generate the vector of trials from uniform PDF
+uniformTrials(i) = customrand(a,b); % Between -2,1
 
-% Generate the normal PDF vector of length TRIALS
-nPDF(i) = customrandn(1.5,2.0); % Mean = 1.5, Std = 2.0
+% Generate the vector of trials from normal PDF
+normalTrials(i) = customrandn(m,s); % Mean = 1.5, Std = 2.0
 
 end
 
 %% Plot PDFs with histograms
-% The histograms are normalized by probability density function estimate
+% Uniform PDF
+uLimits = [a-1,b+1]; % Set x axis limits for plots
+syms x1;
+uPDF = piecewise(uLimits(1)<=x1<-2, 0, a<=x1<=b, 1/(b-a), 1<x1<=uLimits(2), 0); % = 1/(b-a), [-2,1], = 0, otherwise
+% Normal PDF
+nLimits = [m-4*s,m+4*s]; % nPDF(x2 = m+-4*s) <= 0.035, which looks good in most cases 
+syms x2;
+nPDF = (1/[sqrt(2*pi)*s])*exp(-[x2-m]^2/[2*s^2]);
+
+% Two figures with 2 subplots each
 figure;
-subplot(2,1,1), plot(uPDF), title('Uniform PDF'), xlabel('Trial'), ylabel('Value');
-subplot(2,1,2), histogram(uPDF, 'Normalization', 'pdf'), title('Histogram'), xlabel('Value'), ylabel('Probability');
+subplot(2,1,1), fplot(uPDF), title('Uniform PDF, limits(-2,1)'), xlabel('Trial'), ylabel('Value');
+subplot(2,1,2), histogram(uniformTrials, 'Normalization', 'pdf'), xlim(uLimits), title(sprintf('Histogram of %d Trials', trials)), xlabel('Value'), ylabel('Probability');
 figure;
-subplot(2,1,1), plot(nPDF), title('Normal PDF'), xlabel('Trial'), ylabel('Value');
-subplot(2,1,2), histogram(nPDF, 'Normalization', 'pdf'), title('Histogram'), xlabel('Value'), ylabel('Probability');
+subplot(2,1,1), fplot(nPDF, nLimits), title('Normal PDF, Mean=1.5, Std=2.0'), xlabel('Trial'), ylabel('Value');
+subplot(2,1,2), histogram(normalTrials, 'Normalization', 'pdf'), xlim(nLimits), title(sprintf('Histogram of %d Trials',trials)), xlabel('Value'), ylabel('Probability');
